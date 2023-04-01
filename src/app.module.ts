cc8390753146d15eb8279d2ad92fefc1e35cf64a
tarikwaleed <takizee53@gmail.com>
2023-04-01 16:44:58 +0200

    basic setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/salah_back', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log("Connected to the Database");
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { morganMiddleware } from './middlewares/morgan.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(morganMiddleware);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
 }
 bootstrap();
